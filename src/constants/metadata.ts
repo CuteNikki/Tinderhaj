f84e9362a7932c85f0698b20722e01c5b81ee3b7
Nikki <bravonikki13@gmail.com>
2024-12-23 17:39:35 +0100

    Update overscroll color and enhance metadata with Open Graph support

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -3,14 +3,47 @@ import { Metadata } from 'next';
 export const defaultData: Metadata = {
   title: 'Tinderhaj',
   description: 'A dating app like Tinder for Blåhaj.',
+  openGraph: {
+    url: 'https://tinderhaj.com',
+    images: [
+      {
+        url: 'https://tinderhaj.com/opengraph-image.gif',
+        width: 96,
+        height: 96,
+        alt: 'Tinderhaj',
+      },
+    ],
+  },
 };
 
 export const homeData: Metadata = {
   title: 'Tinderhaj - Home',
   description: 'A dating app like Tinder for Blåhaj.',
+  openGraph: {
+    url: 'https://tinderhaj.com',
+    images: [
+      {
+        url: 'https://tinderhaj.com/opengraph-image.gif',
+        width: 96,
+        height: 96,
+        alt: 'Tinderhaj',
+      },
+    ],
+  },
 };
 
 export const notFoundData: Metadata = {
   title: 'Tinderhaj - 404',
   description: "Oops! You've swum too far! Looks like this page got lost in the deep sea.",
+  openGraph: {
+    url: 'https://tinderhaj.com',
+    images: [
+      {
+        url: 'https://tinderhaj.com/opengraph-image.gif',
+        width: 96,
+        height: 96,
+        alt: 'Tinderhaj',
+      },
+    ],
+  },
 };
