5ea47285c28e3795be8b41e822b82cb9b168a171
Nikki <bravonikki13@gmail.com>
2024-12-28 14:06:57 +0100

    Formatted files

--- src/features/home/components/feature-card.tsx
+++ src/features/home/components/feature-card.tsx
@@ -3,7 +3,7 @@ export function FeatureCard({ icon, title, description }: { icon: React.ReactNod
     <div className='rounded-lg bg-card p-8 text-center text-card-foreground'>
       <div className='mb-2 flex justify-center'>{icon}</div>
       <h4 className='font-semibold'>{title}</h4>
-      <p className='text-muted-foreground text-pretty'>{description}</p>
+      <p className='text-pretty text-muted-foreground'>{description}</p>
     </div>
   );
 }
