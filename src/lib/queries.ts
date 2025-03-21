9b03572a9ae617ba238ae7eec946e8b58df846a2
Nikki <bravonikki13@gmail.com>
2025-03-21 15:02:57 +0100

    Removed unnecessary delay from user query functions

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -8,7 +8,6 @@ import { Prisma } from '@prisma/client';
 export const QUERIES = {
   filterUsers: async (query: string, page: number, take: number) => {
     await connection();
-    await new Promise((resolve) => setTimeout(resolve, 1000));
     const where: Prisma.UserWhereInput = {
       OR: [{ username: { contains: query, mode: 'insensitive' } }, { display_name: { contains: query, mode: 'insensitive' } }],
     };
@@ -20,7 +19,6 @@ export const QUERIES = {
 
   getUsers: async (page: number, take: number) => {
     await connection();
-    await new Promise((resolve) => setTimeout(resolve, 1000));
     return {
       users: await prisma.user.findMany({ skip: (page - 1) * take, take: take, orderBy: { username: 'desc' } }),
       count: await prisma.user.count(),
