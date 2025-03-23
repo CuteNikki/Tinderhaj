17d2d7a406d8ff445a50280c1b4f43df370e312c
Nikki <bravonikki13@gmail.com>
2025-03-23 19:31:57 +0100

    Updated user model and seed data

---
+++ prisma/migrations/20250323142834_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "displayName" TEXT NOT NULL,
+    "avatarUrl" TEXT NOT NULL,
+    "bannerUrl" TEXT NOT NULL,
+    "birthday" TIMESTAMP(3) NOT NULL,
+    "pronouns" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "interests" TEXT[],
+    "bio" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
