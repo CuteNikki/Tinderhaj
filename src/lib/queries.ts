679186bd8995e21911c2a1919be552d1ae726a9d
Nikki <bravonikki13@gmail.com>
2025-03-23 20:37:27 +0100

    Enhance user filtering by adding bio, location, and interests to query criteria

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -11,7 +11,13 @@ export const QUERIES = {
 
   filterUsers: async (query: string, page: number, take: number) => {
     const where: Prisma.UserWhereInput = {
-      OR: [{ username: { contains: query, mode: 'insensitive' } }, { displayName: { contains: query, mode: 'insensitive' } }],
+      OR: [
+        { username: { contains: query, mode: 'insensitive' } },
+        { displayName: { contains: query, mode: 'insensitive' } },
+        { bio: { contains: query, mode: 'insensitive' } },
+        { location: { contains: query, mode: 'insensitive' } },
+        { interests: { has: query } },
+      ],
     };
     return { users: await prisma.user.findMany({ skip: (page - 1) * take, take: take, where: where }), totalUsers: await prisma.user.count({ where }) };
   },
