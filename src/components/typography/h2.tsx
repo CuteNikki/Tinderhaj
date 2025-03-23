6ba75fd1a3d1379ec4edc1fa58b169d148c419b0
Nikki <bravonikki13@gmail.com>
2025-03-23 19:31:24 +0100

    Added and modified a few components

--- src/components/typography/h2.tsx
+++ src/components/typography/h2.tsx
@@ -1,5 +1,5 @@
 import { cn } from '@/lib/utils';
 
 export function TypographyH2({ children, className }: { children: React.ReactNode; className?: string }) {
-  return <h2 className={cn('scroll-m-20 text-3xl font-semibold tracking-tight', className)}>{children}</h2>;
+  return <h2 className={cn('scroll-m-20 text-3xl font-bold tracking-tight', className)}>{children}</h2>;
 }
