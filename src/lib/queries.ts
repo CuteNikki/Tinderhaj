e608ad2a88540aa33ba1e1533fc8bfb6319a1319
Nikki <bravonikki13@gmail.com>
2025-03-23 19:29:25 +0100

    Removed unnecessary connection calls and added getUserCount

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,21 +1,22 @@
 import 'server-only';
 
-import { connection } from 'next/server';
-
-import prisma from '@/lib/prisma';
 import { Prisma } from '@prisma/client';
 
+import prisma from '@/lib/prisma';
+
 export const QUERIES = {
+  getUserCount: async () => {
+    return await prisma.user.count();
+  },
+
   filterUsers: async (query: string, page: number, take: number) => {
-    await connection();
     const where: Prisma.UserWhereInput = {
-      OR: [{ username: { contains: query, mode: 'insensitive' } }, { display_name: { contains: query, mode: 'insensitive' } }],
+      OR: [{ username: { contains: query, mode: 'insensitive' } }, { displayName: { contains: query, mode: 'insensitive' } }],
     };
     return { users: await prisma.user.findMany({ skip: (page - 1) * take, take: take, where: where }), totalUsers: await prisma.user.count({ where }) };
   },
 
   getUsers: async (page: number, take: number) => {
-    await connection();
-    return { users: await prisma.user.findMany({ skip: (page - 1) * take, take: take, orderBy: { username: 'desc' } }), totalUsers: await prisma.user.count() };
+    return { users: await prisma.user.findMany({ skip: (page - 1) * take, take: take }), totalUsers: await prisma.user.count() };
   },
 };
