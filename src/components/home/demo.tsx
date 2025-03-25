1e4d77e2be586095edb796149b864cc34ff49d5a
Nikki <bravonikki13@gmail.com>
2025-03-25 14:30:22 +0100

    Added buttons to all 3 demo cards

--- src/components/home/demo.tsx
+++ src/components/home/demo.tsx
@@ -66,29 +66,39 @@ export function Demo() {
         <div className='relative flex items-center justify-center pt-12'>
           {/* Left card */}
           <ProfileCard
-            className='z-0 -mr-14 hidden -rotate-6 transition-transform hover:z-20 hover:scale-110 hover:rotate-2 lg:block'
+            className='z-0 mt-12 -mr-14 hidden -rotate-6 transition-transform hover:z-20 hover:scale-110 hover:rotate-1 lg:block'
             profile={demoProfiles[1]}
-          />
+          >
+            <CardButtons />
+          </ProfileCard>
           {/* Center card */}
           <ProfileCard className='z-10 transition-transform sm:-rotate-2 sm:hover:scale-110 sm:hover:rotate-0' profile={demoProfiles[0]}>
-            <CardFooter className='flex flex-row items-center justify-center gap-6 pb-4'>
-              <Button size='icon' variant='outline' className='h-12 w-12 rounded-full'>
-                <X className='h-6 w-6' />
-                <span className='sr-only'>Dislike</span>
-              </Button>
-              <Button size='icon' className='h-12 w-12 rounded-full'>
-                <Heart className='h-6 w-6' />
-                <span className='sr-only'>Like</span>
-              </Button>
-            </CardFooter>
+            <CardButtons />
           </ProfileCard>
           {/* Right card */}
           <ProfileCard
-            className='z-0 -ml-14 hidden rotate-6 transition-transform hover:z-20 hover:scale-110 hover:rotate-2 sm:block'
+            className='z-0 mt-12 -ml-14 hidden rotate-6 transition-transform hover:z-20 hover:scale-110 hover:rotate-2 sm:block'
             profile={demoProfiles[2]}
-          />
+          >
+            <CardButtons />
+          </ProfileCard>
         </div>
       </div>
     </section>
+  );
+}
+
+function CardButtons() {
+  return (
+    <CardFooter className='flex flex-row items-center justify-center gap-6 pb-4'>
+      <Button size='icon' variant='outline' className='h-12 w-12 rounded-full'>
+        <X className='h-6 w-6' />
+        <span className='sr-only'>Dislike</span>
+      </Button>
+      <Button size='icon' className='h-12 w-12 rounded-full'>
+        <Heart className='h-6 w-6' />
+        <span className='sr-only'>Like</span>
+      </Button>
+    </CardFooter>
   );
 }
