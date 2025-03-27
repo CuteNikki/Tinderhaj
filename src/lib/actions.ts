23a624a1dbcb03ded3abe12de71020d9a81e7d77
Nikki <bravonikki13@gmail.com>
2025-03-27 19:58:37 +0100

    Removed more leftover auth stuff

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,11 +1,6 @@
 'use server';
 
-import { signIn } from '@/lib/auth';
 import { prisma } from '@/lib/prisma';
-
-export async function logIn(provider: 'discord' | 'github' | 'google') {
-  await signIn(provider, { redirectTo: '/' });
-}
 
 export async function deleteProfile() {
   // get username from current session
