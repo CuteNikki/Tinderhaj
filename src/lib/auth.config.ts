2b78c27ffa9afebbb86c6cc736610213ee26bd78
Nikki <bravonikki13@gmail.com>
2025-03-27 19:27:05 +0100

    can't get this to work, idk...

--- src/lib/auth.config.ts
+++
@@ -1,7 +0,0 @@
-import Discord from 'next-auth/providers/discord';
-import GitHub from 'next-auth/providers/github';
-import Google from 'next-auth/providers/google';
-
-import type { NextAuthConfig } from 'next-auth';
-
-export default { providers: [Discord, GitHub, Google] } satisfies NextAuthConfig;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,13 @@
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
 import NextAuth from 'next-auth';
+import Discord from 'next-auth/providers/discord';
+import GitHub from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
 
-import authConfig from './auth.config';
-
-const prisma = new PrismaClient();
+import { prisma } from '@/lib/prisma';
+import { PrismaAdapter } from '@auth/prisma-adapter';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  providers: [Discord, GitHub, Google],
   session: { strategy: 'jwt' },
-  ...authConfig,
 });
