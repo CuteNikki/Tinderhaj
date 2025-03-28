4e26d448fd40371b88c6e088de5a2c8f1d10af5b
Nikki <bravonikki13@gmail.com>
2025-03-28 20:48:13 +0100

    Removed log out page

--- src/app/(auth)/log-out/page.tsx
+++
@@ -1,8 +0,0 @@
-import { LogOutButton } from '@/components/auth/logout-button';
-import { getCurrentUser } from '@/lib/actions';
-
-export default async function LogOutPage() {
-  await getCurrentUser({ redirectIfNotFound: true });
-
-  return <LogOutButton />;
-}
