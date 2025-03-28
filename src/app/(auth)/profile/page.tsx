6a6b485e64d20702dbeec2fb3cec7fe153abf797
Nikki <bravonikki13@gmail.com>
2025-03-28 21:46:02 +0100

    Added preview profile

--- src/app/(auth)/profile/page.tsx
+++ src/app/(auth)/profile/page.tsx
@@ -3,6 +3,7 @@ import { getCurrentUser } from '@/lib/actions';
 import { ProfileForm } from '@/components/auth/profile-form';
 import { Footer } from '@/components/common/footer';
 import { Navbar } from '@/components/common/navbar';
+import { ProfileCard } from '@/components/discovery/profile-card';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 
 export default async function ProfilePage() {
@@ -13,9 +14,9 @@ export default async function ProfilePage() {
       <Navbar />
 
       <main className='flex-1'>
-        <section id='hero' className='bg-muted w-full py-24'>
-          <div className='container mx-auto flex items-center justify-center px-4 md:px-8'>
-            <Card className='max-w-2xl'>
+        <section id='hero' className='bg-muted w-full py-12'>
+          <div className='container mx-auto flex flex-wrap gap-2 items-center justify-evenly px-4'>
+            <Card className='max-w-xl'>
               <CardHeader className='text-center'>
                 <CardTitle className='pt-2 text-xl font-bold'>Customize Profile</CardTitle>
                 <CardDescription className='text-pretty'>Change any details below to personalize your profile!</CardDescription>
@@ -24,6 +25,7 @@ export default async function ProfilePage() {
                 <ProfileForm profile={session.Profile} />
               </CardContent>
             </Card>
+            <ProfileCard profile={session.Profile} />
           </div>
         </section>
       </main>
