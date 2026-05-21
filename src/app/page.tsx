55d61e3654f4aad5b9cf37c637e34b5872c3ec41
Nikki <bravonikki13@gmail.com>
2026-05-21 10:03:53 +0200

    Added shadcn with themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
+import { ThemeSwitch } from '@/components/theme/switch';
+
 export default function Home() {
   return (
     <div className='flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
@@ -43,6 +45,7 @@ export default function Home() {
           >
             Documentation
           </a>
+          <ThemeSwitch />
         </div>
       </main>
     </div>
