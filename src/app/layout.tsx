d58ccfddcbb3a443a37897bf2cdef0fc8c3442a7
Nikki <bravonikki13@gmail.com>
2026-05-21 10:53:09 +0200

    feat: add prettier-plugin-tailwindcss for improved formatting and styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`} suppressHydrationWarning>
-      <body className='min-h-full flex flex-col'>
+      <body className='flex min-h-full flex-col'>
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
